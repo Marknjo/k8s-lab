80c184943ae81725b951568dcf4e1bac9a7e4fff
Marknjo <marknjo7@gmail.com>
2022-09-23 13:55:25 +0300

    fix(kub-test): nodemon not reloading

--- kub-nodejs-app/app.js
+++ kub-nodejs-app/app.js
@@ -1,17 +1,25 @@
-const express = require("express");
+// import express, { Request, Response } from "express";
+import express from "express";
+import morgan from "morgan";
 
 const app = express();
 
+app.use(morgan("combined"));
+
 app.get("/", (req, res) => {
+  // app.get("/", (req: Request, res: Response) => {
   res.send(`
-    <h1>Hello from this NodeJS app!</h1>
+    <h1>Hello from this NodeJS app(update)!!</h1>
     <p>Try sending a request to /error and see what happens</p>
     <p>This lines is the first update of this test container</p>
   `);
 });
 
 app.get("/error", (req, res) => {
+  // app.get("/error", (req: Request, res: Response) => {
   process.exit(1);
 });
 
-app.listen(8080);
+app.listen(8080, () => {
+  console.log("Server running on http://localhost");
+});
