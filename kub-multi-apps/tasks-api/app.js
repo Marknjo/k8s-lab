0ea812bff41fe152162cd5f004d3b9eb92d65fc4
Marknjo <marknjo7@gmail.com>
2022-10-11 15:30:14 +0300

    ci(net): add cors for tasks api

--- kub-multi-apps/tasks-api/app.js
+++ kub-multi-apps/tasks-api/app.js
@@ -2,6 +2,7 @@ import path from "path";
 import fs from "fs";
 
 import express from "express";
+import cors from "cors";
 import axios from "axios";
 import { fileURLToPath } from "url";
 
@@ -11,6 +12,8 @@ const __dirname = path.dirname(__filename);
 const filePath = path.join(__dirname, process.env.TASKS_FOLDER, "tasks.txt");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
