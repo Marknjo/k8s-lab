a0f740bac46f38075779aa049fd40b4d073b4607
Marknjo <marknjo7@gmail.com>
2022-10-12 15:19:34 +0300

    chore(net): finalized mastering kubenetes networking - PV,PVC,Ingress,Nginx,Skaffold,Services&Deployments

--- kub-multi-apps/frontend/src/App.js
+++ kub-multi-apps/frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   const fetchTasks = useCallback(function () {
-    fetch("http://multi-apps.io/tasks", {
+    fetch("/api/tasks", {
       headers: {
         Authorization: "Bearer abc",
       },
@@ -29,7 +29,7 @@ function App() {
   );
 
   function addTaskHandler(task) {
-    fetch("http://multi-apps.io/tasks/create/task", {
+    fetch("/api/tasks/create/task", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
